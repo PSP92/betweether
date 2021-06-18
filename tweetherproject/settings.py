c96df8550ff53b33e8130c1d48627fe185e88523
PSP92 <78216088+PSP92@users.noreply.github.com>
2021-06-18 05:45:37 -0500

    added cors

--- tweetherproject/settings.py
+++ tweetherproject/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'tweether.apps.TweetherConfig',
     'rest_framework',
     'django.contrib.postgres',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
